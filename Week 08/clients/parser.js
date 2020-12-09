19901660bbe936cced33f02de12f3a7149563097
ylm3112370004 <wintersweety@126.com>
2020-12-09 11:26:12 +0800

    补充selfClosing tag 的emit(currentToken);

--- Week 08/clients/parser.js
+++ Week 08/clients/parser.js
@@ -285,6 +285,7 @@ function unQuotedAttributeValue(c) {
 function selfClosingStartTag(c) {
   if (c == ">") {
     currentToken.isSelfClosing = true;
+    emit(currentToken);
     return data;
   } else if (c == EOF) {
 
